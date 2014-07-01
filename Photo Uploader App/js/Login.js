937467ac032db15f84598b57514e908949fcd9e0
Sean Ludtke <sludtke@trihydro.com>
2014-07-01 16:56:17 -0500

    Added Login page and site selection page.
    Changes made by Sean Ludtke (sludtke@trihydro.com).

--- Photo Uploader App/js/Login.js
+++ Photo Uploader App/js/Login.js
@@ -1,1 +1,78 @@
 var token;
+
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady(){
+    
+}
+
+function LoginUser(){
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1;
+    var yyyy = today.getFullYear();
+    if(dd<10){dd='0'+dd} if(mm<10){mm='0'+mm} today = mm+'/'+dd+'/'+yyyy;
+    
+    var userName = $("#txbUsername").val();
+    var passWord = $("#txbPassword").val();
+    
+    $.ajax({
+       type: "GET",
+       url: "https://monoservicetest.trihydro.com/MobileLogin/MobileLoginService.svc/LoginUser",//http://localhost:58478/MobileLoginService.svc/LoginUser
+       data: { userName: userName, password: passWord},
+       dataType: "json",
+       success: function(data){
+           //Update these to real values...
+           $('#divLogin').hide();
+		   $('#divSiteSelection').show();
+           if(data.d != null && data.d != -1){
+               token = data.d;
+               if(document.createElement("datalist").options) {
+ 
+        		$("#ddlSites").on("input", function(e) {
+        			var val = $(this).val();
+        			if(val === "") return;
+        			//You could use this to limit results
+        			//if(val.length < 3) return; 
+        			console.log(val);
+        			$.ajax({
+                    	type:"GET",
+                        url: "http://monoservicetest.trihydro.com/MobilePhoto/PhotoService.svc/GetProjects",
+                        data: {userKey:userName},
+                        dataType: "json",
+                        success: function(data){
+                            var dataList = $("#searchResults");
+                            dataList.empty();
+                            if(data.d.length){
+                                var options = '';
+                                for(var i = 0, len = data.d.length; i<len; i++){
+                                    options += '<option value="'+data.d[i].PortalName+'" />';
+                                    dataList.innerHtml = options;
+                                    //var opt = $("<option></option>").attr("value", data.d[i].PortalName);
+                                    //dataList.append(opt);
+                                }
+                            }
+                        }
+                    });
+                    
+        		});
+         
+        	}
+           }
+           else{
+               $("#txbUsername").val("");
+               $("#txbPassword").val("");
+               $("#divError").text("There was an error logging please try again.");
+           }
+               
+       },
+       error : function(data){
+           alert("There was an error with your login, please try again.");
+       }
+    });
+}
+
+function forceNext(){
+    $('#divSiteSelection').hide();
+    $('#divUploader').show();
+}
