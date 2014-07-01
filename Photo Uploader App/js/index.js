48f5ff771196cf0d8972e2565e97606a5a271792
Keith Ellington <kellington@trihydro.com>
2014-07-01 09:26:04 -0500

    Changes made by Keith Ellington (kellington@trihydro.com). Started prototype. Can take/fetch pictures and exit app (I think)

--- Photo Uploader App/js/index.js
+++ Photo Uploader App/js/index.js
@@ -16,35 +16,123 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-        navigator.splashscreen.hide();
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
+document.addEventListener("deviceready", onDeviceReady, false);
+ 
+function id(element) {
+    return document.getElementById(element);
+}
+
+function onDeviceReady() {
+	cameraApp = new cameraApp();
+    document.getElementById("deviceready").Style = "Block";
+    cameraApp.run();
+}
+
+function cameraApp(){}
+
+cameraApp.prototype={
+    _pictureSource: null,
+    
+    _destinationType: null,
+    
+    run: function(){
+        var that=this;
+        that._pictureSource = navigator.camera.PictureSourceType;
+        that._destinationType = navigator.camera.DestinationType;
+        id("capturePhotoButton").addEventListener("click", function(){
+                    that._capturePhoto.apply(that,arguments);
+                });
+        id("getPhotoFromLibraryButton").addEventListener("click", function(){
+                    that._getPhotoFromLibrary.apply(that,arguments)
+                });
+        id("getPhotoFromAlbumButton").addEventListener("click", function(){
+                    that._getPhotoFromAlbum.apply(that,arguments);
+                });
+        id("btnExit").addEventListener("click", function(){
+			that._showConfirm.apply(that,arguments);            
+        });
+    },
+    
+    _showConfirm: function() {
+        navigator.notification.confirm(
+            'Exit Photo Tool?',  // message
+             closeApp(),              // callback to invoke with index of button pressed
+            'Close App?',            // title
+            'Yes,No'          // buttonLabels
+        );
+        
+    	function closeApp(){
+        	if (navigator.app && navigator.app.exitApp) {
+        		navigator.app.exitApp(); //Android
+        	} else if (navigator.device && navigator.device.exitApp) {
+        		navigator.device.exitApp(); //Iphone
+            }
+    	}
+	},
+    
+    _capturePhoto: function() {
+        var that = this;
+        
+        // Take picture using device camera and retrieve image as base64-encoded string.
+        navigator.camera.getPicture(function(){
+            that._onPhotoDataSuccess.apply(that,arguments);
+        },function(){
+            that._onFail.apply(that,arguments);
+        },{
+            quality: 50,
+            destinationType: that._destinationType.DATA_URL
+        });
+    },
+    
+    _getPhotoFromLibrary: function() {
+        var that= this;
+        // On Android devices, pictureSource.PHOTOLIBRARY and
+        // pictureSource.SAVEDPHOTOALBUM display the same photo album.
+        that._getPhoto(that._pictureSource.PHOTOLIBRARY);
+    },
+    
+    _getPhotoFromAlbum: function() {
+        var that= this;
+        // On Android devices, pictureSource.PHOTOLIBRARY and
+        // pictureSource.SAVEDPHOTOALBUM display the same photo album.
+        that._getPhoto(that._pictureSource.SAVEDPHOTOALBUM)
+    },
+    
+    _getPhoto: function(source) {
+        var that = this;
+        // Retrieve image file location from specified source.
+        navigator.camera.getPicture(function(){
+            that._onPhotoURISuccess.apply(that,arguments);
+        }, function(){
+            cameraApp._onFail.apply(that,arguments);
+        }, {
+            quality: 50,
+            destinationType: cameraApp._destinationType.FILE_URI,
+            sourceType: source
+        });
+    },
+    
+    _onPhotoDataSuccess: function(imageData) {
+         var smallImage = document.getElementById('smallImage');
+         smallImage.style.display = 'block';
+    
+         // Show the captured photo.
+         smallImage.src = "data:image/jpeg;base64," + imageData;
+     	document.getElementById("pOut").innerHTML = "Image Capture Successful";
+    },
+    
+    _onPhotoURISuccess: function(imageURI) {
+        var smallImage = document.getElementById('smallImage');
+        smallImage.style.display = 'block';
+         
+        // Show the captured photo.
+        smallImage.src = imageURI;
+        document.getElementById("pOut").innerHTML = "Image Capture Successful";
+    },
+    
+    _onFail: function(message) {
+       alert(message);
     }
-};
+}
